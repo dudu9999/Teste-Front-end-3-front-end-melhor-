ea1804c156b8a26a37d7b7cb5a1475cd7ab103a5
dudu9999 <ecaetanocorrea@gmail.com>
2019-04-10 17:50:26 -0300

    Criação de front-end melhorado v.1.0

---
+++ README.txt
@@ -0,0 +1,34 @@
+
+Pequeno teste Programação
+=========================
+
+Sua tarefa é implementar o frontend para uma função de pesquisa de uma resposta do aquivo `.JSON`
+
+O frontend deve consistir de um campo onde o usuário pode inserir a consulta de encomendas ainda não entregues.
+
+O HTML deve ser processado no navegador sem recarregar a página.
+
+É importante que a página seja leve, rápida e responsiva, imagine que os clientes usem conexões ruins de internet.
+
+Não há nenhum layout específico disponível, mas o resultado deve ser intuitivo, e não desleixado.
+
+Requisitos
+------------
+
+
+ * Deve seguir as boas práticas (CSS e JS)
+ * O resultado deve exibir os seguintes dados
+  - Número de ordem e nome do cliente
+  - Data do pedido
+  - Valor do pedido
+  - Se entregue ou não
+ * Deve haver uma versão mobile
+ * A versão mobile só deve apresentar os seguintes dados
+  - Número de ordem e nome do cliente
+  - Data do pedido
+  - Se entregue ou não
+ * O pedido da busca deve ser um GET para "/encomendas"
+ * Insira seu nome completo no rodapé da página
+
+
+Escreva o código no arquivo "index.html" que foi enviado no .zip
---
+++ js/script.js
@@ -0,0 +1,100 @@
+﻿// Função para capturar eventos
+function capturaEventos(obj, evt, fn) {
+	// Verifica se o objeto suporta addEventListener
+	if(obj.addEventListener){
+		obj.addEventListener(evt, fn, true);
+		
+	} 
+	// Adiociona attachEvent da Microsoft
+	else {
+		var evento = 'on' + evt;
+		obj.attachEvent(evento, fn);
+	}
+}
+
+// Função para capturar a requisição XMLHttpRequest
+function verificaXmlHttp() {
+	// Uma variável sem valor
+	var xmlhttp;
+	// Verifica se suporta XMLHttpRequest
+	if (window.XMLHttpRequest) {
+		// Adiciona o valor à variável
+		xmlhttp = new XMLHttpRequest();
+	} else {
+		// Adiciona ActiveXObject da Microsoft 
+		xmlhttp = new ActiveXObject('Microsoft.XMLHTTP');
+	}
+	// Retorna o valor
+	return xmlhttp;
+}
+
+// Captura evento load da página
+capturaEventos(window, 'load', function(evt){
+	// Localiza o link com id "a"
+	var button = document.getElementById('button');
+	
+	// Captura o evento de clique neste link
+	capturaEventos(button, 'click', function(evt){
+		var xmlhttp = verificaXmlHttp();
+
+		// Verifica os estados da requisição
+		xmlhttp.onreadystatechange = function(){
+			// Verifica se a página foi carregada corretamente
+			if(xmlhttp.readyState === 4 && xmlhttp.status === 200) {
+				var dadosJSON;
+				try {
+					dadosJSON = JSON.parse(xmlhttp.responseText);
+				} catch(e) {
+					eval("dadosJSON = (" + xmlhttp.responseText + ");");
+				}
+
+				// Localiza nossa div dentro do HTML
+				var div = document.getElementById('texto');
+
+				// Utiliza um laço for ... in
+				for( var propriedade in dadosJSON ){
+					// Adiciona a propriedade no texto da div
+					div.innerHTML += propriedade + ' : ';
+					
+					// Verifica se a propriedade é um objeto
+					if (typeof dadosJSON[propriedade] !== 'object') { 
+						// Adiciona o valor da propriedade no texto da div
+						div.innerHTML += dadosJSON[propriedade] + '<br>';
+					} else { 	             
+						// Se for objeto, acessa o valor da maneira alterativa e 
+						// adiciona na div
+						for(i = 0; i < 10; i++) {
+							div.innerHTML += '<br><hr>';
+							div.innerHTML += 'Número da ordem: ' + dadosJSON[propriedade][i].numero;
+							div.innerHTML += '<br>';
+							div.innerHTML += 'Data do pedido: ' + dadosJSON[propriedade][i].data;
+							div.innerHTML += '<br>';
+							div.innerHTML += 'Valor do pedido: ' + dadosJSON[propriedade][i].valor;
+							div.innerHTML += '<br>';
+							div.innerHTML += 'Status: ' + dadosJSON[propriedade][i].entregue;
+							div.innerHTML += '<br>';
+							//div.innerHTML += 'Data do pedido: ' + dadosJSON[propriedade][i].data; += '<br>';
+							//div.innerHTML += '<br>Cliente:<br>';
+							//div.innerHTML += 'Nome da Empresa: ' + dadosJSON[propriedade][i].cliente.nome;
+							//div.innerHTML += 'Id da Empresa: ' + dadosJSON[propriedade][i].cliente.id;
+							//div.innerHTML += '<br>';
+							div.innerHTML += '<hr>';	
+
+						}						
+					}
+				
+				}				
+			}
+		}
+
+		
+		// Abre a requisição com o método e url
+		xmlhttp.open('GET', 'server.json', true);
+		// Modifica o MimeType da requisição
+		//xmlhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+		// Envia os valores
+		xmlhttp.send(null);
+		
+
+	});
+});
